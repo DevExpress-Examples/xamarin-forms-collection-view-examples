697457605bca7e1a3b6d265fb938a8a5a800bfef
Elena Dvoryadkina <elena.dvoryadkina@devexpress.com>
2020-11-13 11:48:56 +0300

    Selection example

---
+++ CS/02-PullToRefresh/CollectionView_PullToRefresh/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿using Xamarin.Forms;
+
+namespace CollectionView_PullToRefresh {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+            BindingContext = new ViewModel(new MailMessageRepository());
+        }
+    }
+}
