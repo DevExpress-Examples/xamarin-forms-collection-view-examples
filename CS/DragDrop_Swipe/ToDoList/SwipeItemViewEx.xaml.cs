67b06a7a9cd15b05a6725ec7b4decddd47bbdbd2
Elena Dvoryadkina <elena.dvoryadkina@devexpress.com>
2020-11-27 22:07:07 +0300

    Added the DragDrop_Swipe example

---
+++ CS/DragDrop_Swipe/ToDoList/SwipeItemViewEx.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Xamarin.Forms;
+
+namespace ToDoList {
+    public partial class SwipeItemViewEx : SwipeItemView {
+        public static readonly BindableProperty LabelTextProperty = BindableProperty.Create("LabelText",
+            typeof(string), typeof(SwipeItemViewEx), defaultValue: string.Empty,
+            propertyChanged: LabelTextPropertyChanged);
+
+        public static readonly BindableProperty ImageSourceProperty = BindableProperty.Create("ImageSource",
+            typeof(ImageSource), typeof(SwipeItemViewEx), defaultValue: null,
+            propertyChanged: ImageSourcePropertyChanged);
+
+        static void LabelTextPropertyChanged(BindableObject bindable, object oldValue, object newValue) { }
+        static void ImageSourcePropertyChanged(BindableObject bindable, object oldValue, object newValue) { }
+
+        public SwipeItemViewEx() {
+            InitializeComponent();
+            this.swipeItemContainer.BindingContext = this;
+        }
+
+        public string LabelText {
+            get => (string)GetValue(LabelTextProperty);
+            set => SetValue(LabelTextProperty, value);
+        }
+
+        public ImageSource ImageSource {
+            get => (ImageSource)GetValue(ImageSourceProperty);
+            set => SetValue(ImageSourceProperty, value);
+        }
+    }
+}
---
+++ CS/DragDrop_Swipe/ToDoList/ViewModel/DragDropModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ToDoList {
+    public class DragDropModel : NotificationObject {
+        readonly EmployeeTaskRepository repository;
+
+        public IList<EmployeeTask> ItemSource => this.repository.EmployeeTasks;
+
+        public DragDropModel(EmployeeTaskRepository repository) {
+            this.repository = repository;
+        }
+    }
+}
